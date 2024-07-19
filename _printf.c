19c716bdad5e1a7ffdecd97f48b85a4bb681588c
Fabri <oviedofabrizzio1998@gmail.com>
2024-07-19 16:07:31 -0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,43 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <srdarg.h>
+
 int _printf(const char *format, ...)
 {
+  va_list a;
+  int i;
+  int b = 0;
+
+  va_start(a, format);
+
+  for (i = 0; format[i] != '\0'; i++)
+  {
+    if (format[i] == '%')
+    {
+      i++;
+      switch (format[i])
+      {
+        case 'c':
+          b += print_char(a);
+          break;
+        case 's':
+          b += print_string(a);
+          break;
+        case '%':
+          b += print_porcentaje(a);
+          break;
+        default:
+          b += _write_char('%');
+          b += _write_char(format[i]);
+      }
+    }
+    else
+    {
+      b += _write_char(format[i]);
+    }
+  }
+  va_end(a);
+  return (i);
 }
+          
