deec25738706f98f977873e112a7fb8001faa204
Fabri <oviedofabrizzio1998@gmail.com>
2024-07-19 15:32:43 -0300

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef main
 #define main
 
+#include <stdarg.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
+int print_char(va_list a);
+int print_string(va_list a);
+int print_porcentaje(va_list a);
+int _write_char(char c);
 
 #endif
