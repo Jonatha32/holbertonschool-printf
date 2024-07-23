35df351fde42093930b397a47d42793f8f7138cb
Fabri <oviedofabrizzio1998@gmail.com>
2024-07-23 10:26:23 -0300

    Update print_int.c

--- print_int.c
+++ print_int.c
@@ -11,20 +11,20 @@
 
 int print_int(va_list a)
 {
-	int n = va_arg(a, int);
-	int l = get_digit(n);
-	char *b = (char *)malloc(l + 2);
+	int n = va_arg(a, int); //obtengo el entero de la lista de argumentos
+	int l = get_digit(n); //contamos los digitos del numero
+	char *b = (char *)malloc(l + 2); //1 para el signo y otro para el '\0'
 	char *s;
 	int i = 0;
 
 	if (b == NULL)
 		return (-1);
-	itoa(n, b);
+	itoa(n, b); //convertimos el entero a cadena
 	s = b;
 
 	while (*s != '\0')
 	{
-		write(1, s, 1);
+		write(1, s, 1); //usamos write para imprimir cada caracter
 		s++;
 		i++;
 	}
